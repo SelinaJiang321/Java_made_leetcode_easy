b807be648f449f61e799012c898851dbd566a504
Selina Jiang <selinajiang321@gmail.com>
2021-08-21 11:24:50 +1000

    //swap the rows from up to down then swap the symmetry

---
+++ medium/from 1 to 100/48. Rotate Image.java
@@ -0,0 +1,70 @@
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+Example 3:
+
+Input: matrix = [[1]]
+Output: [[1]]
+Example 4:
+
+Input: matrix = [[1,2],[3,4]]
+Output: [[3,1],[4,2]]
+ 
+
+Constraints:
+
+matrix.length == n
+matrix[i].length == n
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+
+
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        //first swap the rows
+        //then swap the elements via symmetry
+      
+      /*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+        int s = 0;
+        int e = matrix.length-1;
+        while (s < e) {
+            int[]temp = matrix[s];
+            matrix[s] = matrix[e];
+            matrix[e] = temp;
+            s++;
+            e--;
+        }
+        
+        //swap through the symmetry
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+    }
+}
