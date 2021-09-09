3e712440037e00c08a7a90a0d0f9ee99e6b01b3e
Selina Jiang <selinajiang321@gmail.com>
2021-09-09 22:29:01 +1000

    //two pointers

---
+++ easy/from 801 to 900/896. Monotonic Array.java
@@ -0,0 +1,54 @@
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+Example 4:
+
+Input: nums = [1,2,4,5]
+Output: true
+Example 5:
+
+Input: nums = [1,1,1]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-105 <= nums[i] <= 105
+*/
+
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+}
