3196970a697d0decb4b58695c28347166dd3bde5
Selina Jiang <selinajiang321@gmail.com>
2021-09-26 21:21:13 +1000

    //how could you forget binary search?! right = pivot - 1; left = pivot + 1;

---
+++ easy/from 701 to 800/704. Binary Search.java
@@ -0,0 +1,57 @@
+/*
+
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+
+
+*/
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) {
+            if (nums[0] != target){
+                return -1;
+            }   
+            else{
+                return 0;
+            }
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = 0;
+        while (left <= right ){
+            middle = left + (right - left) / 2;
+            if (nums[middle] == target) return middle;
+            if (nums[middle] > target){
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            } 
+        }
+        
+        return -1;
+        
+    }
+}
