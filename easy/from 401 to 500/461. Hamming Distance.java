28f6209503237e86d1e2966808771988a1aa7d7a
Selina Jiang <selinajiang321@gmail.com>
2021-11-19 18:45:50 +1100

    // bit operation-> XOR == 1 needs to add an bracket

--- easy/from 401 to 500/461. Hamming Distance.java
+++ easy/from 401 to 500/461. Hamming Distance.java
@@ -42,3 +42,20 @@ class Solution {
         
     }
 }
+
+
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int res = 0;
+        while (x != 0 || y != 0) {
+            if (((x & 1) ^ (y & 1)) == 1) {
+                res ++;
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return res;
+        
+    }
+}
