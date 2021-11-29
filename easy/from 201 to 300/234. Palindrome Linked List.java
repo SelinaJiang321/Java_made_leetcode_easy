644daf82ae3d50716bfbbfb256de2625515812f3
Selina Jiang <selinajiang321@gmail.com>
2021-11-29 21:59:14 +1100

    //need to check the reversed list

---
+++ easy/from 201 to 300/234. Palindrome Linked List.java
@@ -0,0 +1,76 @@
+/*
+Given the head of a singly linked list, return true if it is a palindrome.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if (fast != null) {
+            slow = slow.next;
+        }
+        
+        slow = reverse(slow);
+        ListNode current = head;
+        
+        while (slow != null) {
+            if (slow.val != current.val) {
+                return false;
+            }
+            
+            slow = slow.next;
+            current = current.next;
+        }
+        
+        return true;
+    }
+    
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null, current = head, next;
+        
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+}
