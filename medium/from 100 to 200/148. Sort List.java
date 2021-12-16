c99622a07a46fc1bc88a9a286527b9f337ad6455
Selina Jiang <selinajiang321@gmail.com>
2021-12-16 22:34:16 +1100

    //get the midnode, sort them and then combine them-> divide and conquer

---
+++ medium/from 100 to 200/148. Sort List.java
@@ -0,0 +1,89 @@
+/*
+
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+public ListNode sortList(ListNode head) {
+        /*
+        merge sort
+        T: O(nlogn)
+        S: O(1)
+        */
+        if(head == null || head.next == null){
+            return head;
+        }
+        //T: O(log n) for number of calls
+        ListNode mid = getMid(head);
+        ListNode start = sortList(head);
+        ListNode end = sortList(mid);
+        return merge(start, end);
+    }
+    
+    ListNode merge(ListNode start, ListNode end){
+        ListNode dummy = new ListNode();
+        ListNode head = dummy;
+        while(start != null && end != null){
+            if(start.val <= end.val){
+                dummy.next = start;
+                start = start.next;
+                dummy = dummy.next;
+            } else {
+                dummy.next  = end;
+                end  = end.next;
+                dummy = dummy.next;
+            }
+        }
+        dummy.next = (start == null)? end : start;
+        return head.next;
+    }
+    
+    ListNode getMid(ListNode head){
+        ListNode midPrev = null;
+        while(head !=null && head.next != null){
+            midPrev = (midPrev == null)? head:midPrev.next;
+            head = head.next.next;
+        }
+        ListNode temp = midPrev;
+        midPrev = midPrev.next;
+        temp.next = null;
+        return midPrev;
+    }
+}
