6e4738ebdea83b45c3e774c6ffd750aa997118e6
Selina Jiang <selinajiang321@gmail.com>
2021-12-17 22:13:46 +1100

    //brute force loop through all the rows/columns

---
+++ medium/from 201 to 300/221. Maximal Square.java
@@ -0,0 +1,64 @@
+/*
+
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+*/
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+      int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int maxsqlen = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    int sqlen = 1;
+                    boolean flag = true;
+                    while (sqlen + i < rows && sqlen + j < cols && flag) {
+                        for (int k = j; k <= sqlen + j; k++) {
+                            if (matrix[i + sqlen][k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        for (int k = i; k <= sqlen + i; k++) {
+                            if (matrix[k][j + sqlen] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag)
+                            sqlen++;
+                    }
+                    if (maxsqlen < sqlen) {
+                        maxsqlen = sqlen;
+                    }
+                }
+            }
+        }
+        return maxsqlen * maxsqlen;  
+    }
+}
