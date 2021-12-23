0b673b74b0c46a22018ba985ba1eb3c3bc27ebf8
Selina Jiang <selinajiang321@gmail.com>
2021-12-23 22:00:12 +1100

    //Another sliding window

--- medium/from 1 to 100/3. Longest Substring Without Repeating Characters.java
+++ medium/from 1 to 100/3. Longest Substring Without Repeating Characters.java
@@ -47,3 +47,30 @@ class Solution {
     
     }
 }
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> window = new HashMap<>();
+        int left = 0; int right = 0;
+        int res = 0;
+        while ( right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            
+            window.put(c, window.getOrDefault(c,0) + 1);
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                if (window.containsKey(d)){
+                    window.put(d,window.get(d) -1);
+                }
+            }
+            
+            res = Math.max(res,right - left);
+            
+            
+            
+        }
+        return res;
+    }
+}
