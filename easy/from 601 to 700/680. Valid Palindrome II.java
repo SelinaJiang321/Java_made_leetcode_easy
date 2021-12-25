40b68eec3deedf61a40e2c9fac8a2e5208612486
Selina Jiang <selinajiang321@gmail.com>
2021-12-25 19:17:01 +1100

    //need to separate two functions -> to separate out palindrome

---
+++ easy/from 601 to 700/680. Valid Palindrome II.java
@@ -0,0 +1,52 @@
+/*
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+*/
+
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        int l = -1;
+        int r = s.length();
+        while (++l <= --r) {
+            if (s.charAt(l) != s.charAt(r)) {return checkPalindrome(s,l+1,r) || checkPalindrome(s,l,r-1);}
+        } 
+        return true;
+        
+        
+    }
+    
+    public boolean checkPalindrome (String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            else {
+                l++;
+                r--;
+            }
+            
+        }
+        return true;
+    }
+}
