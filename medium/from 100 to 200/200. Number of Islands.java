5e9cc4c265e0fa86ccc2a0f80402f62f11137d85
Selina Jiang <selinajiang321@gmail.com>
2022-01-27 21:06:07 +1100

    //dfs

---
+++ medium/from 100 to 200/200. Number of Islands.java
@@ -0,0 +1,73 @@
+/*
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+
+*/
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length, n = grid[0].length;
+        // traveser grid
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    // find island, island+1
+                    res++;
+                    // dfs
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            // out of region
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        // change (i, j) to sea water
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+}
