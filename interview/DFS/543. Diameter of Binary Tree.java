b3cd7b2e407274ac7e65c9bca88d91f884dd8fae
Selina Jiang <selinajiang321@gmail.com>
2022-04-07 16:52:25 +1000

    //get the depth instead of height makes things easier

--- interview/DFS/543. Diameter of Binary Tree.java
+++ interview/DFS/543. Diameter of Binary Tree.java
@@ -61,3 +61,37 @@ class Solution {
         return Math.max(lh,rh) + 1;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        //only through left node
+        int left = diameterOfBinaryTree(root.left);
+        int right = diameterOfBinaryTree(root.right);
+        int diameter = maxDepth(root.left) + maxDepth(root.right);
+        return Math.max(Math.max(left,right),diameter);
+        
+        
+    }
+    
+    int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+    }
+}
